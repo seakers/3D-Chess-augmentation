3e2408d29d5d7458ce211757eb1f7126736f64c6
davidF2801 <dfornosf@gmail.com>
2024-09-11 15:23:23 -0500

    SpaDes code updated

--- Evaluator Module/SPA_DES/eose/satellites.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-from .orbits import GeneralPerturbationsOrbitState
-
-class Satellite(BaseModel):
-    orbit: GeneralPerturbationsOrbitState = Field(..., description="Initial orbit state.")
-    field_of_view: float = Field(gt=0, le=180, description="Angular instrument field of view.")
