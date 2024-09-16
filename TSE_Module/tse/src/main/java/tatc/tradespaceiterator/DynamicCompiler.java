48c4f063e367e3a58e0c0e598e4486688f82b003
davidF2801 <dfornosf@gmail.com>
2024-09-16 11:24:35 -0500

    Dynamic compiler adjusted

--- TSE_Module/tse/src/main/java/tatc/tradespaceiterator/DynamicCompiler.java
+++ TSE_Module/tse/src/main/java/tatc/tradespaceiterator/DynamicCompiler.java
@@ -2,34 +2,43 @@ package tatc.tradespaceiterator;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collections;
+import java.net.MalformedURLException;
+import java.lang.reflect.Method;
 import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.JavaFileObject;
+import java.util.Arrays;
 
 public class DynamicCompiler {
-    public Class<?> compileAndLoad(String className, String javaCode) throws IOException, ClassNotFoundException {
-        // Write code to file
-        String fileName = className + ".java";
-        Path sourcePath = Paths.get("path/to/generated", fileName);
-        Files.write(sourcePath, javaCode.getBytes(StandardCharsets.UTF_8));
 
-        // Compile source file
+    public Class<?> compileAndLoad(String className, String javaCode) throws Exception {
+        // Write javaCode to a .java file
+        String fileName = className + ".java";
+        String filePath = "path/to/generated/" + fileName;
+        Files.write(Paths.get(filePath), javaCode.getBytes(StandardCharsets.UTF_8));
+
+        // Compile the .java file
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(Collections.singletonList(sourcePath.toFile()));
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromStrings(Arrays.asList(filePath));
         compiler.getTask(null, fileManager, null, null, null, compilationUnits).call();
         fileManager.close();
 
-        // Load compiled class
-        //URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new File("path/to/generated").toURI().toURL()});
-        URLClassLoader classLoader = null;
-        return Class.forName(className, true, classLoader);
+        // Create a File object for the directory containing compiled classes
+        File generatedDir = new File("path/to/generated");
+        URL generatedDirUrl = generatedDir.toURI().toURL();
+        URL[] urls = new URL[]{ generatedDirUrl };
+
+        // Create a new class loader with the directory
+        URLClassLoader classLoader = new URLClassLoader(urls, getClass().getClassLoader());
+
+        // Load the class
+        return classLoader.loadClass(className);
     }
 }
